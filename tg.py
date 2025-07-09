68ba5f66f8a43fc01fb9b0b883d03c60c5a5a304
vasila535 <vraximova535@gmail.com>
2025-07-09 18:02:27 +0500

    Update tg.py
    version 1.2

--- tg.py
+++ tg.py
@@ -23,3 +23,15 @@ async def ask_phone(message:types.Message):
     name = message.text
     user_data[user_id]['name'] = name
     print(2,user_data)
+    button =[[types.KeyboardButton(text='Raqam jomatish',request_contact=True)]]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=button,resize_keyboard=True)
+    await message.answer(f'Iltimos telefon raqamni kiriting',reply_markup=keyboard)
+    await ask_age(message)
+
+async def ask_age(message:types.Message):
+    user_id =message.from_user.id
+    phone = message.text
+    user_data[user_id]['phone']=phone
+    print(3,user_data)
+    await message.answer(f"Yosh kiriting:")
+    await full_data(message)
